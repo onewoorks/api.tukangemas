b4f2386c140eb36bdc20c0391e2769738c41e68d
iwangibrahim <irwanbibrahim@onewoorks.com>
2017-03-30 14:56:40 +0800

    pricing barang adjustment

--- application/models/product.php
+++ application/models/product.php
@@ -8,89 +8,6 @@ class Product_Model extends Common_Model {
         $this->db = new Mysql_Driver();
     }
 
-    public function CreateProduct(array $productInfo) {
-        $sql = "INSERT INTO oc_product "
-                . " (model,sku,upc,ean,jan,isbn,mpn,location,quantity,stock_status_id,image,manufacturer_id,shipping,price,points,tax_class_id,date_available,weight,weight_class_id,length,width,height,length_class_id,subtract,minimum,sort_order,status,viewed,date_added,date_modified,user_id,ring_size)"
-                . " VALUES "
-                . " ("
-                . "'" . $productInfo['model'] . "',"
-                . "'" . $productInfo['sku'] . "',"
-                . "'" . $productInfo['upc'] . "',"
-                . "'" . $productInfo['ean'] . "',"
-                . "'" . (int) $productInfo['jan'] . "',"
-                . "'" . $productInfo['isbn'] . "',"
-                . "'" . $productInfo['mpn'] . "',"
-                . "'" . $productInfo['location'] . "',"
-                . "'" . (int) $productInfo['quantity'] . "',"
-                . "'" . (int) $productInfo['stock_status_id'] . "',"
-                . "'" . $productInfo['image'] . "',"
-                . "'" . (int) $productInfo['manufacturer_id'] . "',"
-                . "'" . (int) $productInfo['shipping'] . "',"
-                . "'" . (double) $productInfo['price'] . "',"
-                . "'" . $productInfo['points'] . "',"
-                . "'" . (int) $productInfo['tax_class_id'] . "',"
-                . "'" . $productInfo['date_available'] . "',"
-                . "'" . (float) $productInfo['weight'] . "',"
-                . "'" . (int) $productInfo['weight_class_id'] . "',"
-                . "'" . (float) $productInfo['length'] . "',"
-                . "'" . (float) $productInfo['width'] . "',"
-                . "'" . (float) $productInfo['height'] . "',"
-                . "'" . (int) $productInfo['length_class_id'] . "',"
-                . "'" . (int) $productInfo['substract'] . "',"
-                . "'" . (int) $productInfo['minumum'] . "',"
-                . "'" . (int) $productInfo['sort_order'] . "',"
-                . "'" . (int) $productInfo['status'] . "',"
-                . "'" . (int) $productInfo['viewed'] . "',"
-                . "'" . $productInfo['date_added'] . "',"
-                . "'" . $productInfo['date_modified'] . "',"
-                . "'" . (int) $productInfo['user_id'] . "',"
-                . "'" . (float) $productInfo['ring_size'] . "'"
-                . ")";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-        return $this->db->getLastId();
-    }
-
-    public function CreateProductDescription(array $productDescription) {
-        $sql = "INSERT INTO oc_product_description"
-                . " (product_id,language_id,name,description,tag,meta_title,meta_description,meta_keyword) VALUES "
-                . " ("
-                . "'" . (int) $productDescription['product_id'] . "',"
-                . "'" . (int) $productDescription['languange_id'] . "', "
-                . "'" . $productDescription['name'] . "', "
-                . "'" . $productDescription['description'] . "', "
-                . "'', "
-                . "'" . $productDescription['meta_title'] . "', "
-                . "'" . $productDescription['meta_description'] . "', "
-                . "''"
-                . ")";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-    }
-
-    public function CreateProductToCategory(array $product) {
-        $sql = "INSERT INTO oc_product_to_category (product_id,category_id) VALUES ('" . (int) $product['product_id'] . "','" . (int) $product['category_id'] . "')";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-    }
-
-    public function CreateProductToStore($productId, $storeId) {
-        $sql = "INSERT INTO oc_product_to_store (product_id,store_id) VALUES ('" . (int) $productId . "', '" . (int) $storeId . "')";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-    }
-
-    public function CreateProductToLayout($productId, $storeId, $layoutId) {
-        $sql = "INSERT INTO oc_product_to_layout (product_id,store_id,layout_id) VALUES ('" . (int) $productId . "', '" . (int) $storeId . "', '" . (int) $layoutId . "')";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-    }
-
     public function ReadStokByNoSiri($statusItem = 10) {
         $sql = "SELECT "
                 . "kod_kategori_Produk, "
@@ -175,17 +92,6 @@ class Product_Model extends Common_Model {
         return $result[0]['jumlah'];
     }
 
-    public function ReadCountRantaiTE(array $dulangRantai) {
-        $sql = "SELECT "
-                . " count(product_id) as jumlah"
-                . " FROM oc_product_to_category"
-                . " WHERE category_id IN (" . implode(',', $dulangRantai) . ")";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-        $result = $this->db->fetchOut('array');
-        return $result[0]['jumlah'];
-    }
 
     public function ReadProductByKategori($kategoriId) {
         $sql = "SELECT no_siri_Produk as no_siri"
@@ -213,41 +119,6 @@ class Product_Model extends Common_Model {
         return $result;
     }
 
-    public function ReadRantaiTE(array $dulangRantai) {
-        $sql = "SELECT CONCAT(p.isbn,p.model) AS no_siri"
-                . " FROM oc_product_to_category c "
-                . " LEFT JOIN oc_product p ON p.product_id=c.product_id"
-                . " WHERE c.category_id IN (" . implode(',', $dulangRantai) . ")"
-                . " AND p.status=1";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-        $result = $this->db->fetchOut('array');
-        return ($result) ? $result : array();
-    }
-
-    public function ReadProductByKategoriTE($kategoriId) {
-        $sql = "SELECT CONCAT(p.isbn,p.model) AS no_siri"
-                . " FROM oc_product_to_category c "
-                . " LEFT JOIN oc_product p ON p.product_id=c.product_id"
-                . " WHERE c.category_id = '$kategoriId' "
-                . " AND p.status=1";
-//        echo $sql.'  ';
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-        $result = $this->db->fetchOut('array');
-        return $result;
-    }
-
-    public function ReadStatisticProductTE($kategoryId) {
-        $sql = "SELECT count(product_id) as jumlah FROM oc_product_to_category WHERE category_id='$kategoryId'";
-        $this->db->connect();
-        $this->db->prepare($sql);
-        $this->db->queryexecute();
-        $result = $this->db->fetchOut('array');
-        return $result[0];
-    }
 
     public function ReadAllProduct() {
         $sql = "SELECT no_siri_Produk FROM data_database";
@@ -323,6 +194,8 @@ class Product_Model extends Common_Model {
                 . "kategori_Produk, "
                 . "kategori_produk_ID, "
                 . "Dulang, "
+                . "code_Supplier, "
+                . "receiving_Status, "
                 . "no_siri_Produk FROM data_database WHERE statusItem=10";
         $this->db->connect();
         $this->db->prepare($sql);
